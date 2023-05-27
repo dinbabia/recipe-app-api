090afa4ad98f4de8e8047d70c8a5650b9201afbc
Din Laurence C. Babia <dinbabia@gmail.com>
2023-05-27 14:43:21 +0800

    Setup Django Admin

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set our Custom User Model in the core app
+AUTH_USER_MODEL = 'core.User'
